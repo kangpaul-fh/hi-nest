20fd0ef3511f7bbe3384895ff2513b9d3ced4a71
paulkang <kbu715@gmail.com>
2021-05-25 18:48:38 +0900

    ✨ Controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,10 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  // 데코레이터와 함수 사이에 빈공간을 두지않아.
+  @Get('/hello')
+  sayHello(): string {
+    return 'hiya everyone';
+  }
 }
