5c30398f3c39fc21ac6602251fead3fdb873cacb
paulkang <kbu715@gmail.com>
2021-05-26 13:22:22 +0900

    🔨 Movies Service part one

---
+++ src/movies/entities/movie.entity.ts
@@ -0,0 +1,8 @@
+// 실제로 entity에 데이터베이스 모델을 만든다.
+
+export class Movie {
+  id: number;
+  title: string;
+  year: number;
+  genres: string[];
+}
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -8,33 +8,38 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
+  // 이 생성자 덕북엔 this.moviesService.getAll() 쓸 수 있다.
+  constructor(private readonly moviesService: MoviesService) {}
+
   @Get()
-  getAll() {
-    return 'This will return all movies.';
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
   }
 
-  @Get('search')
-  search(@Query('year') searchingYear: string) {
-    return `We are searching for a movie made after: ${searchingYear}`;
-  }
+  // @Get('search')
+  // search(@Query('year') searchingYear: string) {
+  //   return `We are searching for a movie made after: ${searchingYear}`;
+  // }
+
   // Search 부분이 @Get('/:id') 보다 밑에 위치하면 nestjs는 search를 id로 판단. -> 문제가됨
 
   @Get('/:id')
-  getOne(@Param('id') movieID: string) {
-    return `This will return one movie with the id ${movieID}`;
+  getOne(@Param('id') movieID: string): Movie {
+    return this.moviesService.getOne(movieID);
   }
 
   @Post()
   create(@Body() movieData) {
-    console.log(movieData);
-    return movieData;
+    return this.moviesService.create(movieData);
   }
   @Delete('/:id')
-  delete(@Param('id') movieID: string) {
-    return `This will delete a movie with the id: ${movieID}`;
+  remove(@Param('id') movieID: string) {
+    return this.moviesService.deleteOne(movieID);
   }
 
   @Patch('/:id')
@@ -51,3 +56,6 @@ export class MoviesController {
 // 3. @Put 은 모든 리소스를 업데이트해. @Patch는 리소스의 일부분만 업데이트
 // 4. @Body 데코레이터는 movieData안의 리퀘스트의 body를 가져오기 위해 쓴거야.
 // 5. 다시한번 강조. 필요한 parameter를 직접 요청.
+
+// *** Single-responsibility principle
+// 하나의 module, class 혹은 function이 하나의 기능은 꼭 책임져야 한다는 것.
